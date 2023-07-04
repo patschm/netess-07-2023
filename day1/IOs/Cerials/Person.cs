4a49ceb4e02ca4e137e69596179a73f7dd3e33db
Patrick <pschmidt@pasit.nl>
2023-07-04 08:34:23 +0200

    day1

---
+++ day1/IOs/Cerials/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace Cerials;
+
+[XmlType("person")]
+public class Person
+{
+    [XmlAttribute("id")]
+    public int Id { get; set; }
+    [XmlElement("name")]
+    public string? Name { get; set; }
+    [XmlElement("age")]
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {Name} ({Age})";
+    }
+}
