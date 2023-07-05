2d07ad4efb4e7231ccd6d21de856fe31b653be71
Patrick <pschmidt@pasit.nl>
2023-07-05 16:12:33 +0200

    day3

---
+++ day3/Confidentieel/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Confidentieel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TestSymmetrisch();
+        TestAsymmetrisch();
+        Console.ReadLine();
+    }
+
+    private static void TestAsymmetrisch()
+    {
+        // Ontvanger
+        var keyGen = RSA.Create();
+        var publicKey = keyGen.ToXmlString(false);
+        var privateKey = keyGen.ToXmlString(true);
+
+
+        // Zender
+        string loveLetter = "Hello World";
+        var alg = RSA.Create();
+        alg.FromXmlString(publicKey);
+        byte[] cipher = alg.Encrypt(Encoding.UTF8.GetBytes(loveLetter), RSAEncryptionPadding.Pkcs1);
+
+
+        // Ontvanger
+        var alg2 = RSA.Create();
+        alg2.FromXmlString(privateKey);
+        byte[] data = alg2.Decrypt(cipher, RSAEncryptionPadding.Pkcs1);
+        System.Console.WriteLine(Encoding.UTF8.GetString(data));
+
+
+    }
+
+    private static void TestSymmetrisch()
+    {
+        // Zender
+        string loveLetter = "Hello World";
+        Aes alg = Aes.Create();
+        alg.Mode = CipherMode.CBC;
+        var key = alg.Key;
+        var iv = alg.IV;
+
+        byte[] cipher;
+        using (var mem = new MemoryStream())
+        {
+            using(var crypt = new CryptoStream(mem, alg.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using (var writer = new StreamWriter(crypt))
+                {
+                    writer.WriteLine(loveLetter);
+                }
+            }
+            cipher = mem.ToArray();
+        }
+
+
+        // Ontvanger
+        Aes alg2 = Aes.Create();
+        alg2.Mode =CipherMode.CBC;
+        alg2.Key = key;
+        alg2.IV = iv;
+        using(var mem2 = new MemoryStream(cipher))
+        {
+            using(var crypt2 = new CryptoStream(mem2, alg2.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                using (var rdr = new StreamReader(crypt2))
+                {
+                    var data = rdr.ReadToEnd();
+                    System.Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
---
+++ day3/Integriteit/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Integriteit;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TestHash();
+        //TestSymmetric();
+        TestAsymmetric();
+        Console.ReadLine();
+    }
+
+    private static void TestAsymmetric()
+    {
+         // Zender
+        var keygen = DSA.Create();
+        // Certificate
+        var publicKey = keygen.ToXmlString(false);
+        var privateKey = keygen.ToXmlString(true);
+
+
+        string loveLetter = "Hello World";
+        SHA1 alg = SHA1.Create();
+        byte[] hash = alg.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+        var dsa = DSA.Create();
+        dsa.FromXmlString(privateKey);
+        
+        byte[] signature = dsa.SignData(hash, HashAlgorithmName.SHA1);
+
+        // Ontvanger
+        SHA1 alg2 = SHA1.Create();
+        byte[] hash2 = alg2.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+        var dsa2 = DSA.Create();
+        dsa2.FromXmlString(publicKey);
+        bool isOk = dsa2.VerifyData(hash2, signature, HashAlgorithmName.SHA1);
+        System.Console.WriteLine(isOk ? "Het is veilig" : "Er is aan gerommeld");
+    }
+
+    private static void TestSymmetric()
+    {
+         // Zender
+        string loveLetter = "Hello World";
+        HMACMD5 alg = new HMACMD5();
+        //var key = alg.Key;
+        alg.Key = Encoding.UTF8.GetBytes("Pa$$w0rd");
+        byte[] hash = alg.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+
+        // Ontvanger
+        HMACMD5 alg2 = new HMACMD5();
+        alg2.Key = Encoding.UTF8.GetBytes("Pa$$w0rd");
+        byte[] hash2 = alg2.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+        System.Console.WriteLine(Convert.ToBase64String(hash2)); 
+        System.Console.WriteLine(Convert.ToBase64String(hash));
+    }
+
+    private static void TestHash()
+    {
+        // Zender
+        string loveLetter = "Hello World";
+        MD5 alg = MD5.Create();
+        byte[] hash = alg.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+
+        // Ontvanger
+        MD5 alg2 = MD5.Create();
+        byte[] hash2 = alg2.ComputeHash(Encoding.UTF8.GetBytes(loveLetter));
+        System.Console.WriteLine(Convert.ToBase64String(hash2)); 
+        System.Console.WriteLine(Convert.ToBase64String(hash));
+
+    }
+}
